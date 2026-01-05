fae6c8792604cb16b0cfced72d8e02fa5e077b30
allent528 <allenthomasm@gmail.com>
2026-01-05 13:51:25 -0500

    fixed deploy error

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use('/questions', questionsRouter);
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
